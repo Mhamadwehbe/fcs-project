96603c35d45d1cde8a18d53962d042423713d002
Mhamadwehbe <mohamadw496@gmail.com>
2024-07-18 16:24:09 +0300

    Printed first questions

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ cities = {}
 
 
 def main_menu():
+    while True:
+        print("Hello! Please enter:")
+        print("1. To go to the drivers' menu")
+        print("2. To go to the cities' menu")
+        print("3. To exit the system")
+
+
